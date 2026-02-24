682e6dda20932c6f4180f4110cbdf1864ab30056
lucas <lucas.lannoy@hotmail.fr>
2026-02-24 11:59:57 +0100

    ajout de champs requie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/signin", "connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "inscription", "/pages/auth/signup.html" , "/js/auth/signup.js"),
     new Route("/account", "mon compte", "/pages/auth/account.html"),
     new Route("/editpassword", "Changement de mot de passe", "/pages/auth/editpassword.html"),
     new Route("/allreservation", "Vos réservations", "/pages/reservation/allreservation.html"),
---
+++ js/auth/signup.js
@@ -0,0 +1,113 @@
+// implementer le js de ma page
+
+const inputNom = document.getElementById("NomInput");
+const inputPrenom = document.getElementById("PrenomInput");
+const inputEmail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const inputValidationPassword = document.getElementById("ValidationPasswordInput");
+const btnValidationInscription = document.getElementById("btn-validation-inscription");
+
+inputNom.addEventListener("keyup",validateForm);
+inputPrenom.addEventListener("keyup",validateForm);
+inputEmail.addEventListener("keyup",validateForm);
+inputPassword.addEventListener("keyup",validateForm);
+inputValidationPassword.addEventListener("keyup",validateForm);
+
+//Function permettant de valider tout le formulaire
+
+function validateForm(){
+   const nomOK = validateRequired(inputNom);
+    const prenomOK = validateRequired(inputPrenom);
+    const emailOK = validateEmail(inputEmail);
+    const passwordOK = validatePassword(inputPassword);
+    const passwordValidationOK = validatePasswordConfirmation(inputPassword, inputValidationPassword);
+
+    if(nomOK && prenomOK && emailOK && passwordOK && passwordValidationOK){
+        btnValidationInscription.disabled = false;
+
+    }
+
+    else{
+      btnValidationInscription.disabled = true;
+    }
+
+
+
+    // validateRequired(inputPassword);
+    // validateRequired(inputValidationPassword);
+
+}
+
+function validateEmail(input){
+    // definir mon regex
+     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+     const mailUser = input.value;
+     if(mailUser.match(emailRegex)){
+
+         input.classList.add("is-valid");
+         input.classList.remove("is-invalid"); 
+         return true;
+
+     }
+
+      else{
+      input.classList.remove("is-valid");
+      input.classList.add("is-invalid");
+      return false;
+    }
+
+
+}
+
+function validatePassword(input){
+    // definir mon regex
+     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+     const passwordUser = input.value;
+     if(passwordUser.match(passwordRegex)){
+
+         input.classList.add("is-valid");
+         input.classList.remove("is-invalid"); 
+         return true;
+
+     }
+
+      else{
+      input.classList.remove("is-valid");
+      input.classList.add("is-invalid");
+      return false;
+    }
+
+
+}
+
+function validatePasswordConfirmation(passwordInput, validationInput){
+    if(passwordInput.value == inputValidationPassword.value){
+        inputValidationPassword.classList.add("is-valid");
+        inputValidationPassword.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        inputValidationPassword.classList.remove("is-valid");
+        inputValidationPassword.classList.add("is-invalid");
+        return false;
+    }
+}
+
+
+function validateRequired(input){
+    if(input.value !=''){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true; 
+
+    }
+
+    else{
+      input.classList.remove("is-valid");
+      input.classList.add("is-invalid");
+      return false;
+      
+
+
+    }
+}
